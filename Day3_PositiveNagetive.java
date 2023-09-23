117c6810fdd0f783fa8ea14dc19ddd1421bffa38
Deepsolnki <145599861+Deepsolnki@users.noreply.github.com>
2023-09-23 16:13:00 +0530

    Add files via upload

---
+++ Day3_PositiveNagetive.java
@@ -0,0 +1,18 @@
+class PositiveNagetive
+{
+       public static void main(String []args)
+       {
+           int a = 0;
+            
+        if(a>=0)
+           
+               {
+                     System.out.println(a+"number is Positive");
+                }
+        else 
+                  
+                 {
+                     System.out.println(a+"number is Negative");
+                }
+         }
+}
