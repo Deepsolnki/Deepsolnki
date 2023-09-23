13a054da639357bf7ebb71746dddd0522111903f
Deepsolnki <145599861+Deepsolnki@users.noreply.github.com>
2023-09-23 16:10:56 +0530

    Add files via upload

---
+++ Day3_DivisibleNumber.java
@@ -0,0 +1,19 @@
+class Divisible
+{
+              public static void main(String []args)
+                            {
+                             
+                                    int x = 15;
+                            if(x%3==0 \\ x%7==0)
+                                      {
+                                            
+                                             System.out.println("Number is Divisible");
+                                        }
+                            else
+
+                                     {        
+                                             System.out.println("Number is not Divisible");
+                                      }
+                             }
+
+}
