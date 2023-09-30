84a932b1407d23bb1a70d6f82c3985328b8d84ff
Deepsolnki <145599861+Deepsolnki@users.noreply.github.com>
2023-09-30 12:38:17 +0530

    Add files via upload

---
+++ Day8_MonthNumber.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+class NumberofDays{
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	int m = sc.nextInt();
+if(m==1)
+{
+	System.out.println(" January = 31 days in January ");
+}
+else if(m==2)
+{
+	System.out.println(" Feburary = 28 days in Feburary ");
+}
+else if(m==3)
+{
+	System.out.println(" March = 31 days in March ");
+}
+else if(m==4)
+{
+	System.out.println(" April = 30 days in April ");
+}
+else if(m==5)
+{
+	System.out.println(" May = 31 days in May ");
+}
+else if(m==6)
+{
+	System.out.println(" June = 30 days in June ");
+}
+else if(m==7)
+{
+	System.out.println(" July = 31 days in July ");
+}
+else if(m==8)
+{
+	System.out.println(" August = 31 days in August ");
+}
+else if(m==9)
+{
+	System.out.println(" September = 30 days in September ");
+}
+else if(m==10)
+{
+	System.out.println(" October = 31 days in October ");
+}
+else if(m==11)
+{
+	System.out.println(" Feburary = 30 days in Feburary ");
+}
+else if(m==12)
+{
+	System.out.println(" November = 31 days in November ");
+}
+else
+{
+	System.out.println(" Invalid Month Number ");
+}
+	}
+}
