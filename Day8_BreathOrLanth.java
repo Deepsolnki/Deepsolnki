74ce863b8a4b19e62875186184d1d7405fa44bd3
Deepsolnki <145599861+Deepsolnki@users.noreply.github.com>
2023-09-30 12:37:51 +0530

    Add files via upload

---
+++ Day8_BreathOrLanth.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class LanthBreath{
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	System.out.println(" Enter the Lanth of Square ");
+	float a = sc.nextFloat();
+
+	System.out.println(" Enter the Breath of Square ");
+	float b = sc.nextFloat();
+if(a==b)
+{
+	System.out.println(" Square ");
+}
+else
+{
+	System.out.println(" Not Square ");
+}
+	}
+}
