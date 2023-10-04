166b65467eb2e5a0439caed71cc75148b350bed2
Deepsolnki <145599861+Deepsolnki@users.noreply.github.com>
2023-10-05 00:38:04 +0530

    Add files via upload

---
+++ Day13_Divisiblenum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Divisible{
+public static void main(String args [])
+{
+	int a=21;
+	if(a%3==0 && a%7==0)
+{
+	System.out.println("YES");
+}
+else
+{
+	System.out.println("NO");
+}
+  }
+}
